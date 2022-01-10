9ca884f6464f62e30312cfeb693512693233b065
Shazeedul <85868041+Shazeedul@users.noreply.github.com>
2022-01-10 23:37:09 +0600

    10/1/22

--- database/migrations/2022_01_09_075404_admin.php
+++ database/migrations/2022_01_09_075404_admin.php
@@ -21,7 +21,7 @@ class Admin extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->tinyInteger('is_admin')->comment('1 = Super Admin, 2 = Sub Admin');
             $table->tinyInteger('status')->comment('0 = Deactive , 1 = Active');
             $table->timestamps();
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use DateTime;
 
 class AdminSeeder extends Seeder
 {
@@ -17,9 +18,14 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => Str::random(10),
+            'f_name' => Str::random(10),
+            'l_name' => Str::random(10),
             'email' => Str::random(10).'@gmail.com',
             'password' => Hash::make('password'),
+            'status' => 1,
+            'is_admin' => 1,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
         ]);
     }
 }
